5ae0ea61acf899cba30a328cc488790241d59828
Hugo McPhee <hugo.mcphee.public@gmail.com>
2023-08-06 00:12:46 +1000

    updated dist

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -38,6 +38,6 @@ export type GetPartialState<T_State> = {
 };
 export type ExtendsString<T> = T extends string ? T : never;
 export type RepondCallback = (frameDuration: number, frameTime: number) => any;
-export type SetRepondState<T_State> = (newState: GetPartialState<T_State> | ((state: DeepReadonly<T_State>) => GetPartialState<T_State>), callback?: RepondCallback) => void;
+export type SetRepondState<T_State> = (newState: GetPartialState<T_State> | ((state: DeepReadonly<T_State>) => GetPartialState<T_State> | undefined), callback?: RepondCallback) => void;
 export type Phase = "subscribe" | "derive";
 export {};
